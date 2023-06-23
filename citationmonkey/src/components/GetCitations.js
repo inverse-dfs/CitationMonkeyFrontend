9c4ebb93f37085df9c9850290dfc47f31413a9ee
Javin <javinmehta52@gmail.com>
2023-06-22 20:22:04 -0400

    added styling for the get citations

--- citationmonkey/src/components/GetCitations.js
+++ citationmonkey/src/components/GetCitations.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Avatar from '@mui/material/Avatar'
 import Button from '@mui/material/Button'
 import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider/Divider';
 import TextField from '@mui/material/TextField'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
@@ -13,13 +14,16 @@ import axios from 'axios';
 
 const defaultTheme = createTheme()
 
+
 export function GetCitations() {
+    const [data, setData] = React.useState(null);
     const handleSubmit = (event) => {
         event.preventDefault()
         const data = new FormData(event.currentTarget)
         const paper_id = data.get('rootid')
         axios.get("http://54.242.252.72/citations/"+paper_id).then(function (response) {
             console.log(response);
+            setData(response.data)
           })
           .catch(function (error) {
             console.log(error);
@@ -67,6 +71,40 @@ export function GetCitations() {
                             Find Linked Papers!
                         </Button>
                     </Box>
+                </Box>
+                <Box
+                    sx={{
+                        marginTop: 2,
+                        width: '100%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    {data &&
+                        data.map((id) => {
+                            let val = "Id: " + id[0] + " \t \t Title: " + id[1] + "\t\t Number of citations:" + id[2]
+                            return (
+                                <Box
+                                    sx={{
+                                        width: '100%',
+                                        mb: 1,
+                                    }}
+                                >
+                                    <Typography>
+                                        <strong>Paper Id: </strong> {id[0]}
+                                    </Typography>
+                                    <Typography>
+                                        <strong>Title: </strong> {id[1]}
+                                    </Typography>
+                                    <Typography>
+                                        <strong># Citations:  </strong> {id[2]}
+                                    </Typography>
+                                    <Divider sx={{ ml: '15px', borderBottomWidth: 2, background: 'black' }} />
+                                </Box>
+                            )
+                        })
+                    }
                 </Box>
             </Container>
         </ThemeProvider>
